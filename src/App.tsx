9474708df64adcc0527db5c850898c978bac5737
eshwar-u <eshwar.umarengan@gmail.com>
2024-06-01 08:21:51 -0700

    updating scripts to have no errors

--- src/App.tsx
+++ src/App.tsx
@@ -20,8 +20,14 @@ function App() {
 }
 
 export default App;
-//set up the database
+//set up the database-
 //change automation scripts that use the database
-//create the rest api/check how to connect it to the react application
-//add things to lambdas/aws
+//create the rest api/check how to connect it to the react application-
+//add things to lambdas/aws-
 //change the layout of the website based on what durga says
+
+//use get api to get dates for bookcontent
+//display them on the website
+//when the user submits an appointment make a user account and update the appointment
+//update the appointment script (put on lambdas)
+//update the email script (put on lambdas)
