6c791a3e8bddc7e2862d269b045933322a3435cd
eshwar-u <eshwar.umarengan@gmail.com>
2024-06-02 11:33:03 -0700

    updating proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/test": "https://iwxclylnoe.execute-api.us-east-2.amazonaws.com",
+    },
+  },
+});
